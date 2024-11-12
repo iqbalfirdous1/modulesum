fa6f247a37d24e9c8c3fa10f63a0f2349d0ae30b
Iqbal Firdousi <iqbalfirdousi88@gmail.com>
2024-11-12 13:29:26 +0530

    init module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/iqbalfirdous1/modulesum
+
+go 1.23.2
---
+++ sum.go
@@ -0,0 +1,17 @@
+package sum
+
+import "fmt"
+
+func doSum() {
+
+	var i int
+	var j int
+	
+
+	fmt.Print("Type your numbers: ")
+	fmt.Scan(&i, &j)
+	sumofNum := i + j
+	fmt.Printf("Sum of the two numbers is %v\n", sumofNum)
+	
+
+}

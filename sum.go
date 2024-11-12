f1bbe4d8887750e84eac9d4abbd6d47a2b259297
Iqbal Firdousi <iqbalfirdousi88@gmail.com>
2024-11-12 13:47:00 +0530

    edited fucntion

--- sum.go
+++ sum.go
@@ -2,16 +2,14 @@ package sum
 
 import "fmt"
 
-func doSum() {
+func DoSum() {
 
 	var i int
 	var j int
-	
 
 	fmt.Print("Type your numbers: ")
 	fmt.Scan(&i, &j)
 	sumofNum := i + j
 	fmt.Printf("Sum of the two numbers is %v\n", sumofNum)
-	
 
 }
